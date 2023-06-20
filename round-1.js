2951986762ea77f9214c118a70a17cad73a2d7af
Jonathan Amsalem <jonathan.amsalem@gmail.com>
2023-06-20 14:27:37 -0400

    added disabled buttons

--- round-1.js
+++ round-1.js
@@ -1,7 +1,20 @@
 import placeholderQuestions from "./scripts/placeholder-questions.js";
+let clickedButton
+let playerOneScore1 = 0
+let playerTwoScore2 = 0
+let playerNum = 1
+let switchVariable = 1
+let qObject
+let question
 
-
-
+let guess = document.querySelector("#guess")
+let pass = document.querySelector("#pass")
+let userInput = document.querySelector(".user-input")
+let playerOneScore = document.getElementById("playerOneScore")
+let playerTwoScore = document.getElementById("playerTwoScore")
+let playerTurn = document.querySelector(".player-turn")
+playerTurn.textContent = "Player 1 Turn"
+let currentPlayer = playerOneScore
 
 
 let natureQuestions = placeholderQuestions.slice(0,10)
@@ -16,42 +29,28 @@ let historyQuestions = placeholderQuestions.slice(40,50)
 
 let generalQuestions = placeholderQuestions.slice(50,60)
 
-
 let buttons = document.querySelectorAll("button")
- let clickedButton
- let playerOneScore1 = 0
- let playerTwoScore2 = 0 
-let guess = document.querySelector("#guess")
-let pass = document.querySelector("#pass")
-let userInput = document.querySelector(".user-input")
-let playerOneScore = document.getElementById("playerOneScore")
-let playerTwoScore = document.getElementById("playerTwoScore")
-let currentPlayer = playerOneScore
-let playerNum = 1
-let switchVariable = 1
 
-let playerTurn = document.querySelector(".player-turn")
-playerTurn.textContent = "Player 1 Turn"
-guess.disabled = true
-pass.disabled = true
+
+
+function disableButtons (event){
+    buttons.forEach(button=>{
+        if (button != event.target) {
+            button.disabled = true;}
+        })}
 
 function buttonClicked(){
+    
     buttons.forEach(button => {
+        button.addEventListener("click", disableButtons)
         button.addEventListener("click", () => {
             button.style.backgroundColor = "black"
-            button.disabled = true
             clickedButton = Array.from(buttons).filter(btn => btn == button)
-            chooseQuestion(clickedButton)
+            chooseQuestion(clickedButton)})})}
 
-        
-        
-        })})
-    }
 
-let qObject
-let question
  function chooseQuestion(button){
-    switch (clickedButton[0].className){
+    switch (button[0].className){
         case "nature":
              qObject = (natureQuestions[Math.floor(Math.random() * 10)])
              question = qObject.question
@@ -83,17 +82,33 @@ let question
              alert(question)
              break;
      } 
-     guess.disabled = false
-     pass.disabled = false
+ 
      checkAnswer(qObject, playerOneScore)
     }
 
-function checkAnswer(question){  
-        pass.addEventListener("click", event => {
-            event.preventDefault()
-            alert(`Other player's turn... ${question.question}`)
-        })
-        guess.addEventListener("click", event =>{
+function checkAnswer(question){
+    let trys = 0
+    switch(switchVariable){
+        case -1:
+            playerTurn.textContent = "Player 2 Turn"
+            playerNum = 2
+            currentPlayer = playerTwoScore
+            break;
+        case 1:
+            playerTurn.textContent = "Player 1 Turn"
+            playerNum = 1
+            currentPlayer = playerOneScore
+            break;
+    }
+
+    pass.addEventListener("click", event => {
+        event.preventDefault()
+        switchVariable = switchVariable * -1
+        alert(`Other player's turn... ${question.question}`)
+    })
+    guess.addEventListener("click", event =>{
+        buttons.forEach(btn => {
+            btn.disabled = false;})
         event.preventDefault()
         console.log(question.answer)
         if (userInput.value.toLowerCase().trim() == question.answer.toLowerCase().trim()){
@@ -111,36 +126,31 @@ function checkAnswer(question){
             }
 
             if (playerOneScore1 >= 1500 || playerTwoScore2 >= 1500){
+                alert("You are moving on to round 2")
+                setTimeout(() => {
                 document.location.href = "round-2.html"
-             }
+                }, 3000);
+                }
         }
         else{
             alert(`Wrong. Other Player's turn. ${question.question}`)
             userInput.value = ""
-            checkAnswer(question)
             switchVariable = switchVariable * -1
-            console.log(switchVariable)
-            if (switchVariable == -1){
-                playerTurn.textContent = "Player 2 Turn"
-                playerNum = 2
-                currentPlayer = playerTwoScore
-             }
-            else if(switchVariable == 1) {
-                playerTurn.textContent = "Player 1 Turn"
-                playerNum = 1
-                currentPlayer = playerOneScore
+            checkAnswer(question)
 
-        
-        }
-    }
+            }
+            question = null 
+        })
 
-        question = null 
-
-
- })
 
 }
- 
+
+
+
 
 
 buttonClicked()
+
+
+//  pass doesnt work
+// cnat diable all buttons
